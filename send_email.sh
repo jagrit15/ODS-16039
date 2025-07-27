cf8d16e8cf9965b8bb412bf6cfecfda98f77f80c
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-07-28 04:51:04 +0530

    Update send_email.sh

--- send_email.sh
+++ send_email.sh
@@ -3,11 +3,11 @@
 SUBJECT="$1"
 BODY="$2"
 TO_EMAIL="jagritpandeer1969@gmail.com"
-FROM_EMAIL="jagritpandeer1969@gmail.com"
+FROM_EMAIL="jpandeer15@gmail.com"
 REGION="us-east-1"
 
 aws ses send-email \
-    --region "$REGION" \
-    --from "$FROM_EMAIL" \
-    --destination "ToAddresses=$TO_EMAIL" \
-    --message "Subject={Data=$SUBJECT,Charset=utf8},Body={Text={Data=$BODY,Charset=utf8}}"
+  --region "$REGION" \
+  --from "$FROM_EMAIL" \
+  --destination "ToAddresses=$TO_EMAIL" \
+  --message "Subject={Data=\"$SUBJECT\",Charset=utf-8},Body={Text={Data=\"$BODY\",Charset=utf-8}}"
