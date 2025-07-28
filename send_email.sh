407d05262c13ce70d80740a4c2efe1f031c826fc
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-07-29 03:48:32 +0530

    Update send_email.sh

--- send_email.sh
+++ send_email.sh
@@ -2,20 +2,14 @@
 
 SUBJECT="$1"
 BODY_FILE="$2"
-TO_EMAIL="Jagritpandeer1969@gmail.com"
+TO_EMAIL="jagritpandeer1969@gmail.com"
 FROM_EMAIL="jpandeer15@gmail.com"
 REGION="us-east-1"
 
-# Read file safely into variable
-BODY=$(cat "$BODY_FILE")
+BODY=$(<"$BODY_FILE")
 
-# Debug log to verify body (remove after testing)
-echo "Email body content:"
-echo "$BODY"
-
-# Send the email using AWS SES
 aws ses send-email \
   --region "$REGION" \
   --from "$FROM_EMAIL" \
   --destination "ToAddresses=$TO_EMAIL" \
-  --message "Subject={Data=\"$SUBJECT\",Charset=utf-8},Body={Text={Data=\"$(echo "$BODY" | sed 's/"/\\"/g')\",Charset=utf-8}}"
+  --message "Subject={Data=\"$SUBJECT\",Charset=utf-8},Body={Text={Data=\"$BODY\",Charset=utf-8}}"
