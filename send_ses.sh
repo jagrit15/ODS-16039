40dbd53e356ae971fcfa9cc50292a80fefea5b97
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-08-10 14:17:46 +0530

    Update send_ses.sh

--- send_ses.sh
+++ send_ses.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # send_ses.sh <subject> <body-file>
+
 SUBJECT="$1"
 BODY_FILE="$2"
-FROM_EMAIL="jpandeer15@gmail.com"    # must be verified in SES
-TO_EMAIL="jagritpandeer1969@gmail.com"          # must be verified unless SES is in production
+
+FROM_EMAIL="$SES_FROM"
+TO_EMAIL="$SES_TO"
+
+BODY_CONTENT=$(sed ':a;N;$!ba;s/\n/\\n/g' "$BODY_FILE") # escape newlines
 
 aws ses send-email \
     --from "$FROM_EMAIL" \
     --destination "ToAddresses=$TO_EMAIL" \
-    --message "Subject={Data=$SUBJECT,Charset=utf8},Body={Text={Data=$(cat $BODY_FILE),Charset=utf8}}" \
+    --message "Subject={Data=$SUBJECT,Charset=UTF-8},Body={Text={Data=$BODY_CONTENT,Charset=UTF-8}}" \
     --region "$AWS_REGION"
