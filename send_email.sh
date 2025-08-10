0242aace495b00490e6186c10d1c2de74aedfe69
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-08-10 13:07:00 +0530

    Update send_email.sh

--- send_email.sh
+++ send_email.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 SUBJECT="$1"
-BODY_FILE="$2"
+BODY="$2"
 TO_EMAIL="jagritpandeer1969@gmail.com"
 FROM_EMAIL="jagritpandeer1969@gmail.com"
 REGION="us-east-1"
 
-BODY_CONTENT=$(cat "$BODY_FILE" | sed 's/"/\\"/g')
+# Remove ANSI escape codes for clean email
+CLEAN_BODY=$(echo "$BODY" | sed -r "s/\x1B\[[0-9;]*[mK]//g")
+
+# Build CodeBuild log URL if inside CodeBuild
+if [ -n "$CODEBUILD_PROJECT" ] && [ -n "$CODEBUILD_BUILD_ID" ]; then
+    CODEBUILD_LOG_URL="https://console.aws.amazon.com/codesuite/codebuild/projects/${CODEBUILD_PROJECT}/build/${CODEBUILD_BUILD_ID}/?region=${REGION}"
+    CLEAN_BODY="${CLEAN_BODY}\n\nCodeBuild Logs: ${CODEBUILD_LOG_URL}"
+fi
 
 aws ses send-email \
     --region "$REGION" \
     --from "$FROM_EMAIL" \
-    --destination "ToAddresses=$TO_EMAIL" \
-    --message "Subject={Data=\"$SUBJECT\",Charset=utf-8},Body={Text={Data=\"$BODY_CONTENT\",Charset=utf-8\"}}"
+    --destination "ToAddresses=${TO_EMAIL}" \
+    --message "Subject={Data=${SUBJECT},Charset=utf8},Body={Text={Data=${CLEAN_BODY},Charset=utf8}}"
