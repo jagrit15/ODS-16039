5867b29b63a2e2746c0f240caa4e9af84fcab2d7
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-08-10 14:29:35 +0530

    Update send_ses.sh

--- send_ses.sh
+++ send_ses.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-# send_ses.sh <subject> <body-file>
+set -e
 
-SUBJECT="$1"
-BODY_FILE="$2"
+# Variables
+AWS_REGION="us-east-1"
+FROM_EMAIL="jpandeer15@gmail.com"     # Must be a verified SES identity
+TO_EMAIL="jagritpandeer1969@gmail.com" # Must also be verified in SES (sandbox mode)
+SUBJECT="CodeBuild Notification"
+BODY_TEXT="Hello! This is a test email from AWS CodeBuild using SES."
+BODY_HTML="<h1>Hello!</h1><p>This is a test email from <b>AWS CodeBuild</b> using SES.</p>"
 
-FROM_EMAIL="$SES_FROM"
-TO_EMAIL="$SES_TO"
-
-BODY_CONTENT=$(sed ':a;N;$!ba;s/\n/\\n/g' "$BODY_FILE") # escape newlines
+echo "Sending email from $FROM_EMAIL to $TO_EMAIL..."
 
 aws ses send-email \
     --from "$FROM_EMAIL" \
     --destination "ToAddresses=$TO_EMAIL" \
-    --message "Subject={Data=$SUBJECT,Charset=UTF-8},Body={Text={Data=$BODY_CONTENT,Charset=UTF-8}}" \
+    --message "Subject={Data=$SUBJECT,Charset=utf8},Body={Text={Data=$BODY_TEXT,Charset=utf8},Html={Data=$BODY_HTML,Charset=utf8}}" \
     --region "$AWS_REGION"
+
+echo "Email sent command executed."
