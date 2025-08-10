91bc3fa6510b3d35f85a13e12d99cd51912d2363
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-08-10 13:30:43 +0530

    Update send_sns.sh

--- send_sns.sh
+++ send_sns.sh
@@ -2,12 +2,7 @@
 # send_sns.sh <subject> <body-file>
 SUBJECT="$1"
 BODY_FILE="$2"
-SNS_TOPIC_ARN="${SNS_TOPIC_ARN:-$1}"   # prefer env var
-
-if [ -z "$SNS_TOPIC_ARN" ]; then
-  echo "SNS_TOPIC_ARN not set"
-  exit 2
-fi
+SNS_TOPIC_ARN="arn:aws:sns:us-east-1:010438474962:mail"  # replace with yours
 
 aws sns publish \
   --topic-arn "$SNS_TOPIC_ARN" \
