3fd9205276c4fa014b04dc51ba9ebaddc9a5e987
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-08-16 09:29:32 +0530

    Update and rename send_ses.sh to send_sns.sh

---
+++ send_sns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SUBJECT="$1"
+BODY_FILE="$2"
+SNS_TOPIC_ARN="arn:aws:sns:us-east-1:010438474962:mail"  # replace with yours
+
+aws sns publish \
+  --topic-arn "$SNS_TOPIC_ARN" \
+  --subject "$SUBJECT" \
+  --message file://"$BODY_FILE"
