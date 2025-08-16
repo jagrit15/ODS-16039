0f3ea543ed2318b9d05bee0dd5d8672c0f30c8e9
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-08-16 09:37:01 +0530

    Update send_sns.sh

--- send_sns.sh
+++ send_sns.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+# send_sns.sh <subject> <body-file>
 SUBJECT="$1"
 BODY_FILE="$2"
-SNS_TOPIC_ARN="arn:aws:sns:us-east-1:010438474962:mail"  # replace with yours
 
 aws sns publish \
   --topic-arn "$SNS_TOPIC_ARN" \
